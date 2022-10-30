6bb2c2ef4ed2f4f5cb2cce5193badbbe7f68e1a3
Nima Poshtiban <Poshtibannima@gmail.com>
2022-10-30 17:02:41 +0330

    add evaluation for integers, booleans & null

--- repl/repl.go
+++ repl/repl.go
@@ -3,9 +3,9 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"interpreter/evaluator"
 	"interpreter/lexer"
 	"interpreter/parser"
-	"interpreter/token"
 	"io"
 )
 
@@ -50,17 +50,16 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
 		}
 	}
 }
 
 func printParseErrors(out io.Writer, errors []string) {
-	io.WriteString(out,C_MINUS_MINUS)
+	io.WriteString(out, C_MINUS_MINUS)
 	io.WriteString(out, "Woops! We ran into some C-- business here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
