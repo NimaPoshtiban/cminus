dbfb2612b2b1b49c21cd4309a95025409df5ebb4
Nima Poshtiban <Poshtibannima@gmail.com>
2023-03-15 13:22:24 +0330

    add line number to repl

--- repl/repl.go
+++ repl/repl.go
@@ -10,32 +10,23 @@ import (
 	"io"
 )
 
-const C_MINUS_MINUS = `                                                      
-                                                       
-CCCCCCCCCCCCC                                  
-CCC::::::::::::C                                  
-CC:::::::::::::::C                                  
-C:::::CCCCCCCC::::C                                  
-C:::::C       CCCCCC                                  
-C:::::C                                                
-C:::::C                                                
-C:::::C               ---------------  --------------- 
-C:::::C               -:::::::::::::-  -:::::::::::::- 
-C:::::C               ---------------  --------------- 
-C:::::C                                                
-C:::::C       CCCCCC                                  
-C:::::CCCCCCCC::::C                                  
-CC:::::::::::::::C                                  
-CCC::::::::::::C                                  
-CCCCCCCCCCCCC                                  
+const C_MINUS_MINUS = `                                                                                                       
+ _____ _     _  ____
+/ ___/| |   | |/ ___| 
+| (___| |__ | |\___ \ 
+\___ \| '_ \| | ___) |
+ ___) | | | | |/ ___/ 
+|____/|_| |_|_/_/                              
 											   `
 const Prompt = "-> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
+	lineNumber := 0
 	for {
-		fmt.Printf("%s", Prompt)
+		lineNumber++
+		fmt.Printf("%d) %s ", lineNumber, Prompt)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -61,9 +52,9 @@ func Start(in io.Reader, out io.Writer) {
 
 func printParseErrors(out io.Writer, errors []string) {
 	io.WriteString(out, C_MINUS_MINUS)
-	io.WriteString(out, "Woops! We ran into some C-- business here!\n")
+	io.WriteString(out, "\nWoops! We ran into some C-- business here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
-		io.WriteString(out, "\t"+msg+"\t")
+		io.WriteString(out, "\t"+msg+"\t\n")
 	}
 }
